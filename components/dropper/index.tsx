ae89419a557d51773e8b106c6df6813d3a1e12b3
rqphy <raphael.ferreira@hetic.net>
2022-09-17 14:30:59 +0200

    :sparkles: update dropper to render on props

--- components/dropper/index.tsx
+++ components/dropper/index.tsx
@@ -2,16 +2,23 @@ import React from "react"
 import s from "./styles.module.scss"
 import cn from "classnames"
 import { Element } from "@components"
+import { IElement } from "@interfaces"
 
 export type IProps = {
     title: string
+    elements: IElement[]
 }
 
-export default function Dropper ({ title }: IProps) {
+export default function Dropper ({ title, elements }: IProps) {
     return (
         <div className={s.dropper}>
             <h2>{title}</h2>
-            <Element title="test"/>
+            {
+                elements.map((el, index) =>
+                (
+                    <Element key={index} title={el.title}/>
+                ))
+            }
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,30 @@ import { Dropper } from '@components'
 import s from "./styles.module.scss"
 import cn from "classnames"
 
+const elements = [
+  {
+    title: 'toto',
+  },
+  {
+    title: 'Hello',
+  },
+  {
+    title: 'World',
+  },
+  {
+    title: 'React',
+  },
+]
+
 const Home: NextPage = () => {
   return (<>
       <Head>
         <title> Simple title </title>
       </Head>
       <main className={s.table}>
-        <Dropper title='Todo' />
-        <Dropper title='In Progress' />
-        <Dropper title='Completed' />
+        <Dropper title='Todo' elements={elements} />
+        <Dropper title='In Progress' elements={elements}/>
+        <Dropper title='Completed' elements={elements} />
       </main>
     </>
   )
