91162c3689fde3a56f64ec476742dee39f122749
rqphy <raphael.ferreira@hetic.net>
2022-09-17 14:26:07 +0200

    :construction: add draggable element

---
+++ components/element/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import s from "./styles.module.scss"
+import cn from "classnames"
+import { IElement } from "@interfaces"
+
+
+export default function Element ({ title }: IElement) {
+    return (
+        <div className={s.element}>{title}</div>
+    )
+}
