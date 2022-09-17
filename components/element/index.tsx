ea768af2fdad7314108b495a6f9a7202c490f47c
rqphy <raphael.ferreira@hetic.net>
2022-09-17 14:51:07 +0200

    :sparkles: add drag on element

--- components/element/index.tsx
+++ components/element/index.tsx
@@ -1,11 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import s from "./styles.module.scss"
 import cn from "classnames"
 import { IElement } from "@interfaces"
 
-
 export default function Element ({ title }: IElement) {
+    const [isDragging, setIsDragging] = useState<boolean>(false)
+
+    const handleDragStart = () =>
+    {
+        console.log('dragStart')
+        setIsDragging(true)
+    }
+    const handleDragEnd = () =>
+    {
+        console.log('dragEnd')
+        setIsDragging(false)
+    }
+
+
     return (
-        <div className={s.element}>{title}</div>
+        <div
+            className={cn(s.element, isDragging ? s.dragging : '')}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+            draggable="true"
+        >
+            {title}
+        </div>
     )
 }
