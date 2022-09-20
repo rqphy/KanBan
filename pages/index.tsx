d67cdfb2ec72bf83a04367382b9afab6e6253fe2
rqphy <raphael.ferreira@hetic.net>
2022-09-20 11:40:45 +0200

    :construction: testing react-beautiful-dnd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,99 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Dropper } from '@components'
 import s from "./styles.module.scss"
 import cn from "classnames"
+import { DragDropContext, Droppable, Draggable, resetServerContext } from 'react-beautiful-dnd'
+import { v4 as uuid} from 'uuid'
+import { useState, useEffect } from 'react'
+import { GetServerSideProps } from "next";
 
-const elements = [
-  {
-    title: 'toto',
-  },
-  {
-    title: 'Hello',
-  },
-  {
-    title: 'World',
-  },
-  {
-    title: 'React',
-  },
+
+
+const itemsFromBackend = [
+  {id: uuid(), content: "First task"},
+  {id: uuid(), content: "Second task"},
+  {id: uuid(), content: "Third task"},
 ]
 
+const columnsFromBackend = {
+  [uuid()]: {
+    name: 'Todo',
+    items: itemsFromBackend
+  }
+}
+
 const Home: NextPage = () => {
-  return (<>
+  const [columns, setColumns] = useState(columnsFromBackend)
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  useEffect(() => {
+    setIsBrowser(process.browser);
+  }, [])
+
+
+  return (
+    <>
       <Head>
         <title> Simple title </title>
       </Head>
-      <main className={s.table}>
-        <Dropper title='Todo' elements={[...elements, ...elements]} />
-        <Dropper title='In Progress' elements={elements}/>
-        <Dropper title='Completed' elements={elements} />
-      </main>
+      {
+        isBrowser ? (
+          <main className={s.table}>
+            <DragDropContext onDropEnd={result => console.log(result)}>
+              {Object.entries(columns).map(([ columnId, column ]) => (
+                <Droppable droppableId={columnId} key={columnId}>
+                  {
+                    (provided, snapshot) =>
+                      (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          className={s.column}
+                        >
+                          {
+                            column.items.map((item, index) =>
+                            (
+                              <Draggable key={item.id} draggableId={item.id} index={index}>
+                                {
+                                  (provided, snapshot) =>
+                                  (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      className={s.item}
+                                      // style={{...provided.draggableProps.style}}
+                                    >
+                                      {
+                                        item.content
+                                      }
+                                    </div>
+                                  )
+                                }
+                              </Draggable>
+                            ))
+                          }
+                        </div>
+                      )
+                  }
+                </Droppable>
+              ))}
+            </DragDropContext>
+          </main>
+        ) : null
+      }
+
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) =>
+{
+
+  resetServerContext()   // <-- CALL RESET SERVER CONTEXT, SERVER SIDE
+
+  return {props: { data : []}}
+
 }
 
 export default Home
