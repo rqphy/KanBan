d20aac4819481c16258aeea69a40930391b45d47
rqphy <raphael.ferreira@hetic.net>
2022-09-20 14:25:58 +0200

    :sparkles: complete logic with 3 columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,53 @@ const columnsFromBackend = {
   [uuid()]: {
     name: 'Todo',
     items: itemsFromBackend
+  },
+  [uuid()]: {
+    name: 'In Progress',
+    items: []
+  },
+  [uuid()]: {
+    name: 'Compledted',
+    items: []
+  },
+}
+
+const onDragEnd = (result, columns, setColumns) =>
+{
+  if(!result.destination) return
+  const { source, destination } = result
+  if(source.droppableId !== destination.droppableId)
+  {
+    const sourceColumn = columns[source.droppableId]
+    const destColumn = columns[destination.droppableId]
+    const sourceItems = [...sourceColumn.items]
+    const destItems = [...destColumn.items]
+    const [removed] = sourceItems.splice(source.index, 1)
+    destItems.splice(destination.index, 0, removed)
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems
+      }
+    })
+  } else
+  {
+    const column = columns[source.droppableId]
+    const copiedItems = [...column.items]
+    const [removed] = copiedItems.splice(source.index, 1)
+    copiedItems.splice(destination.index, 0, removed)
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems
+      }
+    })
   }
 }
 
@@ -39,44 +86,50 @@ const Home: NextPage = () => {
       {
         isBrowser ? (
           <main className={s.table}>
-            <DragDropContext onDropEnd={result => console.log(result)}>
+            <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
               {Object.entries(columns).map(([ columnId, column ]) => (
-                <Droppable droppableId={columnId} key={columnId}>
-                  {
-                    (provided, snapshot) =>
-                      (
-                        <div
-                          {...provided.droppableProps}
-                          ref={provided.innerRef}
-                          className={s.column}
-                        >
-                          {
-                            column.items.map((item, index) =>
-                            (
-                              <Draggable key={item.id} draggableId={item.id} index={index}>
-                                {
-                                  (provided, snapshot) =>
-                                  (
-                                    <div
-                                      ref={provided.innerRef}
-                                      {...provided.draggableProps}
-                                      {...provided.dragHandleProps}
-                                      className={s.item}
-                                      // style={{...provided.draggableProps.style}}
-                                    >
-                                      {
-                                        item.content
-                                      }
-                                    </div>
-                                  )
-                                }
-                              </Draggable>
-                            ))
-                          }
-                        </div>
-                      )
-                  }
-                </Droppable>
+                <div key={columnId}>
+                  <h2>{column.name}</h2>
+                  <Droppable droppableId={columnId}>
+                    {
+                      (provided, snapshot) =>
+                        (
+                          <div
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                            className={s.column}
+                          >
+                            {
+                              column.items.map((item, index) =>
+                              (
+                                <Draggable key={item.id} draggableId={item.id} index={index}>
+                                  {
+                                    (provided, snapshot) =>
+                                    (
+                                      <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                        className={s.item}
+                                        // style={{...provided.draggableProps.style}}
+                                      >
+                                        {
+                                          item.content
+                                        }
+                                      </div>
+                                    )
+                                  }
+                                </Draggable>
+                              ))
+                            }
+                            {
+                              provided.placeholder
+                            }
+                          </div>
+                        )
+                    }
+                  </Droppable>
+                </div>
               ))}
             </DragDropContext>
           </main>
