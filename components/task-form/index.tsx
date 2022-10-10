57268ce66d21e5939331802359d69a305ccd10f9
rqphy <raphael.ferreira@hetic.net>
2022-10-10 11:41:36 +0200

    :lipstick: add form style

--- components/task-form/index.tsx
+++ components/task-form/index.tsx
@@ -8,13 +8,13 @@ export type IProps = {
 
 export default function TaskForm ({ onClick }: IProps) {
     return (
-        <form onSubmit={(_event) => {
+        <form className={s.form} onSubmit={(_event) => {
             _event.preventDefault()
             const formData = new FormData(_event.target)
             const formProps = Object.fromEntries(formData)
             onClick(formProps.title.toString())
         }}>
-            <input type="text" name="title" id="title" placeholder="New Task"/>
+            <input className={s.input} type="text" name="title" id="title" placeholder="New Task"/>
             <button className={s.button}>
                 Add new task
             </button>
