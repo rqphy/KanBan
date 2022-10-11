4d6afe004757a9c6da425c846577e603fadc401a
rqphy <raphael.ferreira@hetic.net>
2022-10-11 11:23:02 +0200

    :wrench: add public folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -136,6 +136,7 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title> Kanban App </title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       {
         isBrowser ? (
